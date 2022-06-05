9f4aac3713aa7826da85620ae89950b83dfcf385
SR-Shahen <s.r.shahenda@gmail.com>
2022-06-05 15:20:39 +0600

    Project setup

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-center font-bold text-teal-400'>Welcome to Green Garden Tools</h1>
+
+        </div>
+    );
+};
+
+export default Home;
