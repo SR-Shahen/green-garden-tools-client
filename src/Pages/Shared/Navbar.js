7045df94ca3c5932a103275e0080d85e2a124355
SR-Shahen <s.r.shahenda@gmail.com>
2022-07-10 00:00:47 +0600

    implement signout functionality

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,12 +1,25 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    const handelSignOut = () => {
+        signOut(auth);
+
+    }
     const menuItems = <>
         <li><Link to='/' className='text-lg font-medium'>Home</Link></li>
         <li><Link to='/about' className='text-lg font-medium'>About Us</Link></li>
         <li><Link to='/contact' className='text-lg font-medium'>Contact Us</Link></li>
-        <li><Link to='/login' className='text-lg font-medium'>Login</Link></li>
+        {
+            user && <li><Link to='/dashboard' className='text-lg font-medium'> Dashboard </Link></li>
+        }
+        <li>{user ? <button className='text-lg font-medium' onClick={handelSignOut}>Log Out</button> :
+            <Link to='/login' className='text-lg font-medium'> Login </Link>}</li>
     </>
     return (
         <div className="navbar bg-fuchsia-400">
